85c78818c7d83d3ea87ee20251ec57b81099636f
blurdel <5648737+blurdel@users.noreply.github.com>
2023-11-02 16:17:58 -0400

    cmake with an added lib, can be static/shared

---
+++ hello.hpp
@@ -0,0 +1,10 @@
+#ifndef HELLO_HPP
+#define HELLO_HPP
+
+namespace hello {
+
+    void sayHello();
+    
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+#include "hello.hpp"
+
+int main()
+{
+    // std::cout << "Hello World!" << std::endl;
+
+    hello::sayHello();
+    
+}
